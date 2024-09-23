19e02eee5591a1d4efa0804fc91234253e578606
Emmanuel Adisa <94123198+bohemiancode-x@users.noreply.github.com>
2024-09-23 20:26:20 +0100

    hero text change.

--- src/components/ui/home/hero/index.js
+++ src/components/ui/home/hero/index.js
@@ -7,7 +7,7 @@ export const HeroSection = () => {
             <div className='mt-auto w-[70%] space-y-5'>
                 <h2 className='uppercase text-2xl text-primary font-[700]'>Robotics & Technology</h2>
                 <h1 className='uppercase tracking-wide text-8xl font-[800]'>We <span className='text-primary'>create</span> the future</h1>
-                <p className='text-lg'>Vestibulum sollicitudin justo vitae leo euismod, eu laoreet diam lobortis. Cras massa risus, hendrerit vel risus sed, cursus fringilla massa. Aenean et finibus lacus. Vestibulum purus ante.</p>
+                <p className='text-lg'>Robolytics LLC stands out with proprietary frameworks that allow us to optimize LLMs for specific domains, significantly reducing costs and enhancing system performance.</p>
             </div>
             <div></div>
         </div>
